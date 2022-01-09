993efee62f42b909bb751430e697a8942466a196
BZMplan <15249938392@163.com>
2022-01-09 12:23:31 +0800

    up

---
+++ src/com/mplan/JD.java
@@ -0,0 +1,16 @@
+package com.mplan;
+
+import javax.swing.JDialog;
+import javax.swing.*;
+
+import java.awt.*;
+public class JD extends JDialog{
+    public JD(){
+        Container c = getContentPane();
+        c.add(new JLabel("这是一个对话框"));
+        setVisible(true);
+        setBounds(600,400,100,100);
+    }
+    
+    
+}
---
+++ src/com/mplan/JF.java
@@ -0,0 +1,38 @@
+package com.mplan;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseWheelListener;
+public class JF<ActionEvent> extends JFrame {
+
+    public JF(){
+        setTitle("这是一个窗体");
+        setVisible(true);//设置窗体可见
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// DISPOSE_ON_CLOSE 释放资源
+        setBounds(600,400,400,400);//设置窗体坐标和大小，像素
+        setResizable(false);
+
+        JLabel l = new JLabel("这是一个窗体");
+        JButton JB = new JButton("按钮");
+
+        Container c = getContentPane();
+        c .setBackground(Color.WHITE);//设置背景颜色
+        c.add(l);//添加组件
+        c.add(JB);
+        c.validate();//验证容器中的组件
+        c.setLayout(new FlowLayout());
+
+        JB.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                new JD();
+            }
+        });
+            
+
+        System.out.print("x="+getX()+"y="+getY());
+    }
+
+    public static void main(String[] args) {
+        new JF();
+    }
+}
